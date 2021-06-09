3114fecb7ffa8b2aa5664c443b0aa81783ac4b39
Troy Martin <beef.erikson.studios@gmail.com>
2021-06-09 00:41:25 -0400

    Add project files.

---
+++ JoeAndBobCash/Guy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoeAndBobCash
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        /// <summary>
+        /// Writes amount of cash to console
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine("{0} has {1} bucks.", Name, Cash);
+        }
+
+        /// <summary>
+        /// Gives cash, removing from wallet if enough, prints error if not
+        /// </summary>
+        /// <param name="amount">Amount of cash to give</param>
+        /// <returns>The amount of cash removed from wallet or 0 if there was not enough cash</returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("{0} says {1} isn't a valid number.", Name, amount);
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine("{0} says: \"I don't have enough cash to give you {1}.\"");
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+
+        /// <summary>
+        /// Receives cash and adds to wallet
+        /// </summary>
+        /// <param name="amount">Amount of money to receive</param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+                Console.WriteLine("{0} says: {1} isn't an amount I'll take.");
+            else
+                Cash += amount;
+        }
+    }
+}
---
+++ JoeAndBobCash/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace JoeAndBobCash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates new Guy objects
+            Guy joe = new Guy() { Cash = 50, Name = "Joe" };
+            Guy bob = new Guy() { Cash = 100, Name = "Bob" };
+
+            while (true)
+            {
+                // Displays guy info
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
+
+                // Prompts for amount
+                Console.Write("Enter an amount: ");
+                string howMuch = Console.ReadLine();
+                if (howMuch == "")
+                    return;
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    // Determines who cash is coming from / going to
+                    Console.Write("Who should give the cash: ");
+                    string whichGuy = Console.ReadLine();
+
+                    if (whichGuy == "Joe")
+                    {
+                        int giveAmount = joe.GiveCash(amount);
+                        bob.ReceiveCash(giveAmount);
+                    }
+                    else if (whichGuy == "Bob")
+                    {
+                        int giveAmount = bob.GiveCash(amount);
+                        joe.ReceiveCash(giveAmount);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter 'Joe' or 'Bob'.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount (or a blank line to exit): ");
+                }
+            }
+        }
+    }
+}
